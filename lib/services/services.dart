79dfe2bc9d4d0eca498f1956db569ff9a3e76d8f
Habiba <habiba.amr2611@gmail,com>
2024-03-04 17:44:00 +0200

    first1

---
+++ lib/services/services.dart
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+import 'package:flutter/material.dart';
+
+
+
+
+Widget customeIimageHomeBage({
+  required String image,
+  required double radius ,
+
+})=>CircleAvatar(
+radius: radius,
+backgroundImage:  AssetImage(
+    image),
+);
+
+
+Widget customeIimage({
+  required String image,
+  required double radius ,
+  int color=0xFF6495ED,
+})=>CircleAvatar(
+  radius: radius,
+  backgroundColor: Color(color),
+  child: Image(
+    image: AssetImage(image),
+  ),
+);
+
+
+Widget customeButton({
+  required void Function()? onPressed,
+  required String text,
+  required context,
+  int color=0xFFA93226,
+}) =>
+    Container(
+      width: double.infinity,
+      height: 50,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(18),
+        color: Color(color),
+      ),
+      child: TextButton(
+        onPressed: onPressed,
+        child: Text(text,
+            style: Theme.of(context).textTheme.displayMedium,
+        ),
+      ),
+    );
+
+
